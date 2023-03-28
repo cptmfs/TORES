5d5f59955c3378359a5dcaf2360162b6218a768b
Ozcan Kara <ozcankara810@gmail.com>
2023-03-28 11:08:33 +0300

    some changes

--- TORES.Wf/LogDataForm.Designer.cs
+++ TORES.Wf/LogDataForm.Designer.cs
@@ -28,22 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgwLog = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwLog)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgwLog
+            // 
+            this.dgwLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwLog.Location = new System.Drawing.Point(-11, 35);
+            this.dgwLog.Name = "dgwLog";
+            this.dgwLog.Size = new System.Drawing.Size(800, 274);
+            this.dgwLog.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(287, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KULLANICI GİRİŞ/ÇIKIŞ EKRANI";
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(148, 315);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(222, 41);
+            this.btnList.TabIndex = 2;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(404, 315);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(214, 41);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
             // 
             // LogDataForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 397);
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgwLog);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "LogDataForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "LogDataForm";
+            this.Text = "Log_Data_Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwLog)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgwLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnClear;
     }
 }
